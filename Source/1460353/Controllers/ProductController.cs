0b9e2ba4d4304a74787d6fcb4be51258ef96d959
lac <lac>
2017-05-29 22:40:15 +0700

    Load San Pham Theo Danh Muc

--- Source/1460353/Controllers/ProductController.cs
+++ Source/1460353/Controllers/ProductController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using _1460353.Models;
 namespace _1460353.Controllers
 {
-    [Filters.Login]
+    //[Filters.Login]
     public class ProductController : Controller
     {
         // GET: Product
@@ -17,6 +17,18 @@ namespace _1460353.Controllers
         public ActionResult New()
         {
             return View();
+        }
+        public ActionResult LoadSPTheoDanhMuc(int? iddm)
+        {
+            if (iddm.HasValue == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (var daugia = new daugiaEntities())
+            {
+                var list = daugia.sanphams.Where(s => s.id_danhmuc == iddm).ToList();
+                return View(list);
+            }
         }
     }
 }
