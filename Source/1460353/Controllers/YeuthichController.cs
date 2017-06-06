9fa1d9b70dda88b18af2f865e1727fc84af706fc
Luhamter09 <buingochoang1996@gmail.com>
2017-06-06 22:43:58 +0700

    load index yeuthich phantrang ajax

--- Source/1460353/Controllers/YeuthichController.cs
+++ Source/1460353/Controllers/YeuthichController.cs
@@ -10,24 +10,30 @@ namespace _1460353.Controllers
     public class YeuthichController : Controller
     {
         // GET: Yeuthich
-        public ActionResult Index(int page=1)
+        public ActionResult Index(int page = 1)
         {
             ViewBag.page = page;
-            ViewBag.pageTotal = Helpers.yeuthich.total() % 4 == 0 ? (Helpers.yeuthich.total() / 4) : (Helpers.yeuthich.total() /4) + 1;
+            ViewBag.pageTotal = Helpers.yeuthich.total() % 4 == 0 ? (Helpers.yeuthich.total() / 4) : (Helpers.yeuthich.total() / 4) + 1;
             var listsanpham = Helpers.yeuthich.loaddanhsachsanpham();
             listsanpham = listsanpham.Skip((page - 1) * 4).Take(4).ToList();
             return View(listsanpham);
         }
 
-        
+        public ActionResult IndexAjax(int page)
+        {
+            var listsanpham = Helpers.yeuthich.loaddanhsachsanpham();
+            listsanpham = listsanpham.Skip((page - 1) * 4).Take(4).ToList();
+            return Json(listsanpham, JsonRequestBehavior.AllowGet);
+        }
 
         public ActionResult themyeuthich(int idsanpham)
         {
-            using(var data=new Models.daugiaEntities()){
+            using (var data = new Models.daugiaEntities())
+            {
                 var yeuthich = new Models.yeuthich()
                 {
-                    id_nguoidung=Helpers.Login.nguoidung().id,
-                    id_sanpham=idsanpham
+                    id_nguoidung = Helpers.Login.nguoidung().id,
+                    id_sanpham = idsanpham
                 };
                 data.yeuthiches.Add(yeuthich);
                 data.SaveChanges();
@@ -38,7 +44,7 @@ namespace _1460353.Controllers
         {
             using (var data = new Models.daugiaEntities())
             {
-                var idnguoidung=Helpers.Login.nguoidung().id;
+                var idnguoidung = Helpers.Login.nguoidung().id;
                 var yeuthich = data.yeuthiches.Where(yt => yt.id_sanpham == idsanpham && yt.id_nguoidung == idnguoidung).FirstOrDefault();
                 data.yeuthiches.Remove(yeuthich);
                 data.SaveChanges();
