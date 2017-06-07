9b5926c16f76a5adbee7208502e754e5b063960c
HienBach <hienbach996@gmail.com>
2017-06-08 00:09:30 +0700

    Top 5 san pham gan het han

--- Source/1460353/Controllers/HomeController.cs
+++ Source/1460353/Controllers/HomeController.cs
@@ -23,13 +23,29 @@ namespace _1460353.Controllers
                 var list = ctx.sanphams.OrderByDescending(p => p.giahientai).Take(5).ToList();
                 return View(list);
             }
+
+        }
+        public PartialViewResult Top5SPGiaCao()
+        {
             using (var ctx = new Models.daugiaEntities())
             {
                 var list = ctx.sanphams.OrderByDescending(p => p.giahientai).Take(5).ToList();
-                return View(list);
+                return PartialView("_Top05SPNhieuDauGia", list);
             }
         }
-
-
-	}
+        public PartialViewResult Top5SPLuotDauCaoNhat()
+        {
+            using (var ctx = new Models.daugiaEntities())
+            {
+               // var list = ctx.lichsudaus.Where(p=> p.id_sanpham.)
+                return PartialView("_SPCoNhieuLuotDaunhat", list);
+            }
+        }
+        public PartialViewResult Top5SPGanKetThuc()
+        {
+            using (var ctx = new Models.daugiaEntities())
+            {
+                var list = ctx.sanphams.OrderByDescending(p => p.ngayketthuc).Take(5).ToList();
+                return PartialView("_Top05SPGanHetHan", list);
+            }
 }
