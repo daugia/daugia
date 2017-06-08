c155213031eb1206d2b6d569735e17e4e736edd7
Luhamter09 <buingochoang1996@gmail.com>
2017-06-08 15:58:54 +0700

    check thong bao da xem

--- Source/1460353/Controllers/ThongbaoController.cs
+++ Source/1460353/Controllers/ThongbaoController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace _1460353.Controllers
 {
+    [Filters.LoginUser]
     public class ThongbaoController : Controller
     {
         // GET: Thongbao
@@ -18,10 +19,31 @@ namespace _1460353.Controllers
             using (var data = new Models.daugiaEntities())
             {
                 var idnguoidung = Helpers.Login.nguoidung().id;
-                var list = data.thongbaos.Where(tt => tt.id_nguoidung == idnguoidung).ToList();
+                var list = data.thongbaos.Where(tt => tt.id_nguoidung == idnguoidung).OrderByDescending(tt=>tt.id).ToList();
                 return PartialView("list",list);
 
             }
         }
+
+        public ActionResult check()//check daxem
+        {
+            using (var data=new Models.daugiaEntities())
+            {
+                var idnguoidung = Helpers.Login.nguoidung().id;
+                var thongbao = data.thongbaos.Where(tt=>tt.daxem==0 && tt.id_nguoidung==idnguoidung);
+                foreach (var tt in thongbao)
+                {
+                    tt.daxem = 1;
+                    data.Entry(tt).State = System.Data.Entity.EntityState.Modified;
+                   
+                }
+                data.SaveChanges();
+
+
+                return Content("11");
+            }
+                
+        }
+
     }
 }
