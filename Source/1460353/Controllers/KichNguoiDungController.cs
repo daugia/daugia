775e6c5f128635a15c2fc80785de7e55b8e55ab1
lac <lac>
2017-06-09 22:18:00 +0700

    thêm tình trạng cho lịch sử đấu và thêm kích người dùng khi giữ giá thì chuyển cho người phía sau

--- Source/1460353/Controllers/KichNguoiDungController.cs
+++ Source/1460353/Controllers/KichNguoiDungController.cs
@@ -11,6 +11,7 @@ namespace _1460353.Controllers
     public class KichNguoiDungController : Controller
     {
         // GET: KichNguoiDung
+        [Filters.LoginUser]
         [HttpPost]
         public ActionResult Index(int? idsp, int? idnd)
         {
@@ -21,6 +22,31 @@ namespace _1460353.Controllers
                 k.id_sanpham = idsp;
                 k.id_nguoidungquanlysp = Login.nguoidung().id;
                 daugia.kichnguoidungs.Add(k);
+                var lsd = daugia.lichsudaus.Where(ls => ls.id_sanpham == idsp && ls.id_nguoidung == idnd).ToList();
+                foreach (var l in lsd)
+                {
+                    l.tinhtrang = 1;
+                }
+                daugia.SaveChanges();
+                var nsp = daugia.sanphams.Where(s => s.id == idsp && s.id_nguoidunghientai == idnd).FirstOrDefault();
+                if (nsp != null)
+                {
+                    var lsds = daugia.lichsudaus.Where(ls => ls.id_sanpham == idsp && ls.tinhtrang == 0).OrderByDescending(ls=>ls.id).FirstOrDefault();
+                    if (lsds == null)
+                    {
+                        nsp.giahientai = nsp.giakhoidiem;
+                        nsp.giacaonhat = nsp.giakhoidiem;
+                        nsp.id_nguoidunghientai = null;
+                    }
+                    else
+                    {
+                        var ndt = daugia.nguoidungs.Where(nd => nd.id == idnd).FirstOrDefault();
+                        ndt.taikhoan = ndt.taikhoan + nsp.giacaonhat;
+                        nsp.giahientai = lsds.tiendadau;
+                        nsp.giacaonhat = lsds.tiendadau;
+                        nsp.id_nguoidunghientai = lsds.id_nguoidung;
+                    }
+                }
                 daugia.SaveChanges();
                 return RedirectToAction("LSSanPham", "LichSuDau", new { id = idsp });
             }
